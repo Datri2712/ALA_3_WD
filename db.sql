3c714409da57013a1e58f49346e43ffcf939369c
Datri2712 <datripandya27@gmail.com>
2025-04-25 10:39:36 +0530

    Create db.sql

---
+++ db.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS bookdb;
+USE bookdb;
+
+CREATE TABLE IF NOT EXISTS books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    published_year INT
+);
